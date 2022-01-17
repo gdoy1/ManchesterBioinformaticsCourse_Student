0bef901415cdb1ec612dfda5a0dbcb3fb730417e
George Doyle <george.doyle@nbt.nhs.uk>
2022-01-17 15:11:35 +0000

    Init

--- Day3/Code/Assignment.py
+++ Day3/Code/Assignment.py
@@ -1,7 +1,35 @@
 """
-Refer to Assignment.pdf for instructions!
-
-Remember - this is assessed. Make sure your code is nicely structured
-
-Comments are not optional
+Image processing in Python: assignment
+Ben Bunce & George Doyle
 """
+
+import matplotlib.pyplot as plt
+import numpy as np
+from skimage.io import imread
+
+image = imread('lungs.jpg', as_gray=True)  # load image into skimage
+cropped = image[90:480,140:480]
+plt.imshow(cropped)
+plt.show()
+
+
+'''fig = plt.figure()
+ax = fig.add_subplot(211)
+ax2 = fig.add_subplot(212)
+ax.imshow(image, interpolation="none", cmap="Greys_r", vmin=0.0, vmax=0.2)
+ax2.hist(image.flatten(), bins=255, facecolor="Red", edgecolor="Black")
+plt.show()'''
+
+# Step 4 - Show histogram
+flat = cropped.flatten()
+plt.hist(flat)
+plt.show()
+
+# Step 5
+def yourfunction(window, level):
+    plt.imshow(cropped, interpolation="none", cmap="Greys_r", vmin=window, vmax=0.8)
+    plt.show()
+    yourfunction(0.2, 0.8)
+    yourfunction(0.4, 0.8)
+    yourfunction(0.6, 0.8)
+    yourfunction(0.8, 0.8)
