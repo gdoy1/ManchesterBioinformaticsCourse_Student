fda1b5e2678d9ff1066f616e2d7e94369ee90a6e
George Doyle <george.doyle@nbt.nhs.uk>
2022-01-20 16:04:28 +0000

    Small fixes to final commit

--- Day3/Code/Assignment.py
+++ Day3/Code/Assignment.py
@@ -22,25 +22,34 @@ flat = cropped.flatten()
 plt.hist(flat)
 plt.show()
 
+'''
+Q5 - Histogram displays level of black/white in the grayscale image. A value of 0 correlates with black, whilst 1
+correlates to white. The intervening values between 0 and 1 represent grayscale colouring.
+'''
+
 # Function to modify window and level of lung image
-def yourfunction(window, level):
+def adjustwindowlevel(window, level):
     # create figure and subplots
     fig = plt.figure()
     ax = fig.add_subplot(211)
     ax2 = fig.add_subplot(212)
     # add lung image
     ax.imshow(cropped, interpolation="none", cmap="Greys_r", vmin=(level-window)/2, vmax=(level+window)/2)
-    # add histogram
-    ax2.hist(cropped.flatten())
+    # save modified lung image into a temporary jpg
+    plt.imsave('lung_temp.jpg', cropped, cmap="Greys_r", vmin=(level-window)/2, vmax=(level+window)/2)
+    # load the modified window/level lung into histo variable
+    histo = imread('lung_temp.jpg', as_gray=True)
+    # add histogram using adjusted image
+    ax2.hist(histo.flatten())
     # set figure text
     plt.figtext(0.5, 0.01, "vmin: " + str(window) + ". vmax: " + str(level), fontsize=10,
                 ha="center", bbox={"facecolor": "grey", "alpha": 0.5, "pad": 5})
     plt.show()
 
 # Tested function with different window/level parameters
-yourfunction(0.01, 1)
-yourfunction(0.4, 1)
-yourfunction(0.6, 0.8)
-yourfunction(0.3, 0.7)
-yourfunction(1, 1)
-yourfunction(1, 0.2)
+adjustwindowlevel(0.01, 1)
+adjustwindowlevel(0.4, 1)
+adjustwindowlevel(0.6, 0.8)
+adjustwindowlevel(0.3, 0.7)
+adjustwindowlevel(1, 1)
+adjustwindowlevel(1, 0.2)
